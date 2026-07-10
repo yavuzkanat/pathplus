268fea9a86a70f909c2466003d96b135eeb262ca
Ahmet <ahmetyavuzkanat@outlook.com>
2026-07-10 11:21:15 +0300

    FileManager:exists method

--- pathplus/file.py
+++ pathplus/file.py
@@ -1,2 +1,22 @@
+from pathlib import Path
+
+
 class FileManager:
-    pass
+    
+    def __init__(self, path : str | Path):
+        
+        self._path = Path(path)
+    
+    @property
+    def exists(self) -> bool:
+        """
+        The method to check whether the file exists.
+        It was created using with **pathlib.Path** 
+        
+        Returns
+        -------
+            True : The file is exists
+            False : The File is **not** exists
+        """
+        
+        return Path.exists(self._path)
