cb5f045f31c10877ca2159daa77f381c38ab1dae
Ahmet <ahmetyavuzkanat@outlook.com>
2026-07-11 11:38:03 +0300

    file_manager: info() method

--- pathplus/file_manager.py
+++ pathplus/file_manager.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from hashlib import sha256
 from hashlib import sha512
 from hashlib import md5
+from datetime import datetime
+import os
 
 class FileManager:
     
@@ -84,5 +86,45 @@ class FileManager:
             raise ValueError(f"Unsupported hash algorithm: {algorithm}") from exc
 
         return hash_function(self._path.read_bytes()).hexdigest()
+    
+    
+    
+    
+    
+    def info(self) -> dict[str, str | Path | int | bytes]:
+        """
+        
+        The method gets infos about the file.
+        
+        Returns
+        -------
+        
+        Info about the file (dict)
+        
+        """
+        infos = {}
+        
+        stat = self._path.stat()
+
+        infos = {
+
+            "name": self._path.name,
+
+            "path": self._path.resolve(),
+
+            "size": stat.st_size,
+
+            "created_time": datetime.fromtimestamp(stat.st_birthtime).strftime("%d/%m/%Y"),
+
+            "extension": self._path.suffix,
             
-      
+            "content": self._path.read_bytes()
+
+        }
+        
+        return infos          
+
+  
+        
+        
+        
